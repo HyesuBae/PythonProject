a5ab9f91a72d407c0358e9aa9fda3001688eb329
HyesuBae <hsb506@gmail.com>
2016-05-18 21:14:43 +0900

    Make models

--- accountBookPjt/accountBook/models.py
+++ accountBookPjt/accountBook/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Expenditure(models.Model):
+    user = models.ForeignKey('auth.User')
+    date = models.DateField()
+    contents = models.CharField(max_length=200)
+    expenditure = models.IntegerField()
+    expend_from = models.CharField(max_length=100)
+    category = models.CharField(max_length=200)
+
+    def set_expenditure(self):
+        self.save()
+
+class Income(models.Model):
+    user = models.ForeignKey('auth.User')
+    date = models.DateField()
+    contents = models.CharField(max_length=200)
+    income = models.IntegerField()
+    income_to = models.CharField(max_length=100)
+    category = models.CharField(max_length=200)
+
+    def set_income(self):
+        self.save()
